ff562b4df490d47268d4798b24c060461abfd537
Benjamin Jz <ben2003@web.de>
2026-07-07 17:40:51 +0100

    reallocated print details for display. also added filters

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 // Konstanten des ESP-Verkabelung-Ultrasonic-Sensor JSP-SR04T-V33
 const int echoPin = 5;
 const int trigPin = 4;
+const int cycle = 2000; // cycle of sensing waterlevel in milliseconds
 
 // Konstanten des JSP-SR04T-Ultraschall-Sensors
 const unsigned int distance_deadzone = 22;  // Deadzone des Sensors (bei dem Ultraschallsensor von JSN-SR04T ists wohl 22cm)
@@ -12,7 +13,7 @@ const long max_TOF_sens = 30000; // grenze des Sensors (maximale Mess-Reichweite
 // Konstanten der Installation in Essencia Wassertank Sensor. Angaben in Zentimeter
 const unsigned int distance_max_depth_watertank = 150; // maximaler, sinnvoller zu messender Wert zwischen Sensor und Tankboden (bzw.) der Wassertank-Tiefe (bis zum Sensor)
 const unsigned int height_watertank_0percent = 10;  // bei 10cm (in Essencia) fängt das Wasserrohr an, darunterliegende Wasserstände können nicht gepumpt werden.
-const unsigned int height_watertank_100percent = 120; // vom Boden, 120cm (in Essencia) ist der Wassertank mit 100% voll betitelt. da der Sensor jedoch 22cm Deadzone hat, ist das mal hier so früh auf 100% definiert. Man müsste den Sensor höher montieren (wie chatgpt/gemini schon gesagt hatte)
+const unsigned int height_watertank_100percent = 120; // vom Boden, 120cm (in Essencia) is der Wassertank mit 100% voll betitelt. da der Sensor jedoch 22cm Deadzone hat, ist das mal hier so früh auf 100% definiert. Man müsste den Sensor höher montieren (wie chatgpt/gemini schon gesagt hatte)
 const unsigned int distance_watertank_0percent = distance_max_depth_watertank - height_watertank_0percent; // 150 - 10 = 140cm Abstand zum Sensor bei leerem Tank
 const unsigned int distance_watertank_100percent = distance_max_depth_watertank - height_watertank_100percent; // 150 - 120 = 30cm Abstand zum Sensor bei vollem Tank
 
@@ -22,7 +23,10 @@ const unsigned int liter_per_cm = 125; // Liter Inhalt pro centimeter: Pi*R*R(de
 
 bool toggle_var = true;
 unsigned long lastTestTriggerTime = 0;
-String text_string = ""; // Variable für Textausgabe deklariert
+String SensorTextPrint = ""; // Variable für Textausgabe deklariert
+String SensorStatus = ""; // Variable für SensorStatus deklariert
+float distance_filtered = 0.0; // Globaler Filterwert
+bool is_first_run = true;      // Flag für Erstinitialisierung des Filters
 
 U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ 21, /* clock=*/ 18, /* data=*/ 17);
 
@@ -59,60 +63,67 @@ void loop() {
   long duration = pulseIn(echoPin, HIGH, max_TOF_sens);
   long distance = duration * 0.034 / 2;
 
-  
-  // if out of range, sonderbehandlung: // auf gefiltertem wert
+  if (duration == 0) {
+    // Sensor somehow disconnected? not sensing anymore!
+    SensorStatus = "Error (Timeout)";
+    SensorTextPrint = "Sensor Plugged?";
+  } else if (distance <= distance_deadzone) {
     // if out of range, dead-zone Sensor (Tank Full?)
-    text_string = "Sensor Deadzone (Closer 22cm). If Tank full, then okey"; 
+    SensorStatus = "OK (Min. Distance)";
+    SensorTextPrint = "dist. > 22cm. If Tank full, then okey"; 
+  } else if (distance > distance_max_depth_watertank) {
+    // if out of distance, too far away, error
+    SensorStatus = "Error (> Max. Distance)";
+    SensorTextPrint = "dist. > 150cm";
+  } else if ( abs(distance/distance_filtered-1.0)>0.05 ) 
+    {  
+    // Filter for opening Lit to have a look.
+    // max accepted change 5% / cycle
+    // would it be better to filter this later? on collected data?
+    SensorStatus = "Error (high Sens drift)";
+    SensorTextPrint = "Lit Opened?";
+  } else {
+    SensorStatus = "OK (-:";
 
-    
-    // if out of range, too far away, error
-    text_string = "detected Range to far";
-
-  if (distance > distance_max_depth_watertank) 
-  {
-    long distance_filtered = 0; 
-    distance_filtered = (distance_filtered/9*10) + distance*(1/10);
+    // Lowpass-Filter generell integrieren (90% Altwert, 10% Neuwert)
+    if (is_first_run) {
+      distance_filtered = distance;
+      is_first_run = false;
+    } else {
+      distance_filtered = (distance_filtered * 0.9) + (distance * 0.1);
+    }
+    SensorTextPrint = (distance); // funktioniert das hier? distance is ja float Und SensorTextPrint ein String...?
   }
+    
+  
     
   // TODOs:
   // Lora aus Chat von Steffen integrieren
-  // Filter (22cm distance? und lowpass-filter generell interieren)
-  // Boundaries, die kritisch sind eintragen:
-     // Distance grösser 150cm - kann nicht sein.
-     // Distance 22cm = Full, Min-Sensor-Value (DeadZone)
-     // Distance 0cm == TimeOut -> verbessern!
 
-
-  // 2. DISPLAY-AUSGABE (Erst nach der Messung starten)
   u8g2.firstPage();
   do {
     u8g2.drawStr(0, 12, "Wassertank-Sensor");
     u8g2.drawHLine(0, 16, 128);
+    u8g2.setCursor(0, 35);
+    u8g2.print("STATUS:");
+    u8g2.print(SensorStatus);
+    u8g2.setCursor(0, 55);
+    u8g2.print(SensorTextPrint);
 
-    if (duration == 0) {
-      u8g2.setCursor(0, 35);
-      u8g2.print("STATUS: TIMEOUT!");
-      u8g2.setCursor(0, 55);
-      u8g2.print("Sensor unplugged?");
-    } 
-    else {
-      u8g2.setCursor(0, 35);
-      u8g2.print("STATUS: OKEY");
-      
-      u8g2.setCursor(0, 55);
-      toggle_var = false; // true wäre für Mess- und Testzwecke. Kann irgendwann ganz entfernt werden, wenn nicht benötigt.
+    u8g2.setCursor(0, 55);
+    toggle_var = false; // für testzwecke
 
       if (toggle_var == true) {
         u8g2.print("UltraSonicTOF: ");
         u8g2.print(duration);
         u8g2.print(" us");
-      } else {
+      } else 
+      {
         u8g2.print("Distanz: ");
         u8g2.print(distance);
         u8g2.print(" cm");
-      }
-    } 
+      } 
   } while ( u8g2.nextPage() );
 
-  delay(2000); 
+  delay(cycle); 
 }
